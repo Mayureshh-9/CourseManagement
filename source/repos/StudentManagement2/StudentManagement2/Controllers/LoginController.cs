ff0f01ab6a1575bf5065603dd3eb3ff8348a4bce
Mayuresh Chikane <mayureshchikane6@gmail.com>
2024-10-06 21:50:34 +0530

    ...

---
+++ source/repos/StudentManagement2/StudentManagement2/Controllers/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using StudentManagement2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentManagement2.Controllers
+{
+    public class LoginController : Controller
+    {
+        private StudinfoEntities db = new StudinfoEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        
+
+        public ActionResult Index(user objchk)
+        {
+            
+                    var obj = db.users.Where(a => a.username.Equals(objchk.username) && a.password.Equals(objchk.password)).FirstOrDefault();
+
+                    if (obj != null)
+                    {
+
+                        Session["UserId"] = obj.id.ToString();
+
+                        Session["Username"] = obj.username.ToString();
+
+                        return RedirectToAction("Index","Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The Username or Password is incorrect");
+                        return View();
+                    }
+
+
+                
+
+
+            
+            
+        }
+        public ActionResult loggedin()
+        {
+            {
+                if (Session["UserId"] != null)
+                {
+                    return RedirectToAction("loggedin","Login");
+                }
+                else
+                { return View(); }
+            }
+        }
+       
+        
+
+    }
+
+}
+
+            
+            
---
+++ source/repos/StudentManagement2/StudentManagement2/Models/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagement2.Models
+{
+    public class Login
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string username { get; set; }
+
+        [Required]
+        public string password { get; set; }
+    }
+}
